928b9c35535c9b7c9896cb666c6b3eccf077efbc
hicham <hicha.taha@gmail.com>
2023-06-27 10:09:01 -0700

    init repo

---
+++ README.md
@@ -0,0 +1,39 @@
+## React Portfolio
+  
+
+## Description
+
+
+## Table of Contents
+
+* [Installation](##Installation)
+* [Usage](##Usage)
+* [Documentation](##Documentation)
+* [Questions](##Questions)
+  
+## Installation
+
+Installation is a process of installing the dependencies required.
+Intialize node package manager and then run the following commands;  
+```script
+npm install
+```  
+
+## Usage
+
+ In order to use the application locallaly, Run the following command:  
+```script
+npm start
+```  
+
+## Documentation
+
+### Demonstration
+
+#### Screenshot
+
+![Screenshot of the Application]()
+
+
+
+  
---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Hicham Raffiai Idrissi</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="" alt="Hicham Raffiai Idrissi" />
+
+                <p>
+                full stack web developer (HTML, CSS (Bootstrap, HandleBars), JavaScript, jQuery, React.js, IndexedDB,
+            Node.js, Express.js, SQL, Sequelize, MongoDB, Mongoose, NoSQL, APIs, Templating.)
+                </p>
+                <p>
+                Quality Assurance Technician
+                </p>
+            
+            </div>
+        </section>
+    )
+}
+
+export default About;
